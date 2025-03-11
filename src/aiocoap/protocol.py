8ba3cd85b9a5025cf922bb76c9aba0bd0b1921f2
Martine Lenders <m.lenders@fu-berlin.de>
2025-03-11 04:04:07 +0100

    Copy `is_inner` attribute to request blocks

--- src/aiocoap/protocol.py
+++ src/aiocoap/protocol.py
@@ -838,6 +838,7 @@ class BlockwiseRequest(BaseUnicastRequest, interfaces.Request):
                 if block_cursor == 0:
                     current_block1.opt.size1 = len(app_request.payload)
                 current_block1.remote = app_remote
+                current_block1.is_inner = app_request.is_inner
             else:
                 current_block1 = app_request
 
